4a6b80d2e98c3e09344cf5719ee820f3d0dba54d
mattno312 <45409183+mattno312@users.noreply.github.com>
2018-12-01 06:39:48 -0800

    holy mo

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import five from "./media/5d.png";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        s
+        <img src={five} className="seize" alt="fire" />
+      </div>
+    );
+  }
+}
+
+export default App;
